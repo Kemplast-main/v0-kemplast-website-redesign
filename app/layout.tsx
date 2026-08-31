cfb771d4285510b3d01a3639db98ec935ea67147
mdecoder24 <krkushwant@gmail.com>
2026-08-31 12:20:57 +0530

    feat: initialize root layout with SEO metadata and structured data schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type React from "react"
-import Script from "next/script"
 import type { Metadata } from "next"
 import { Analytics } from "@vercel/analytics/next"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -300,14 +299,16 @@ export default function RootLayout({
   return (
     <html lang="en" className="light" suppressHydrationWarning>
       <head>
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        {/* Google Tag Manager - must be as high in <head> as possible */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KD3B5QSX');`}
-        </Script>
+})(window,document,'script','dataLayer','GTM-KD3B5QSX');`,
+          }}
+        />
         {/* End Google Tag Manager */}
         <script
           type="application/ld+json"
